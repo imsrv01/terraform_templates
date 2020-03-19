a593ccf656a7bfdab53beafd288fe32a4bf4e7ba
imsrv01 <39862925+imsrv01@users.noreply.github.com>
2020-03-19 02:28:29 -0500

    Update example.tf

--- example/example.tf
+++ example/example.tf
@@ -7,3 +7,13 @@ resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }
+
+module "web_server_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "web-server"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = "vpc-12345678"
+
+  ingress_cidr_blocks = ["10.10.0.0/16"]
+}

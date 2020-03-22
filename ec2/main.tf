b2f97b5fdb721ee9522945aa401612615a2f3ac4
Shantaram Vernekar <shantaram_vernekar@persistent.co.in>
2020-03-22 11:03:12 -0500

    removed interpolation

--- ec2/main.tf
+++ ec2/main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "default" {
 
 # Create a subnet to launch our instances into
 resource "aws_subnet" "default" {
-  vpc_id                  = "${aws_vpc.default.id}"
+  vpc_id                  = aws_vpc.default.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone="us-east-1a"
